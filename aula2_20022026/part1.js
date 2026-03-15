47020091bd3b5e76411342792df3b38c7cdb033a
Elvis Ugwu <elvisugwu18@gmail.com>
2026-03-15 19:52:20 -0300

    part1.js

---
+++ aula2_20022026/part1.js
@@ -0,0 +1,26 @@
+Parte 1
+
+let times = ['Flamengo', 'Palmeiras', 'Corinthians', 'São Paulo', 'Santos', 'Grêmio', 'Internacional', 'Cruzeiro', 'Atlético Mineiro', 'Botafogo', 'Fluminense', 'Vasco', 'Bahia', 'Fortaleza', 'Ceará', 'Sport', 'Athletico Paranaense', 'Coritiba', 'Red Bull Bragantino', 'Juventude', 'Atlético Goianiense'];
+
+A)
+Posição 0: Flamengo
+Posição 7: Cruzeiro
+Posição 11: Vasco
+Posição 15: Sport
+Posição 18: Red Bull Bragantino
+Posição 20: Atlético Goianiense
+
+B)
+Penúltima posição: Juventude
+Última posição: Atlético Goianiense
+
+C)
+21 elementos
+
+D)
+times.push('Chapecoense');
+
+E)
+for (let i = 0; i < times.length; i++) {
+console.log(times[i]);
+}
