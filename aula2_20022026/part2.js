a868a3c44927c03e38eaa8171f610a12273239ca
Elvis Ugwu <elvisugwu18@gmail.com>
2026-03-15 19:53:12 -0300

    part2.js

---
+++ aula2_20022026/part2.js
@@ -0,0 +1,22 @@
+Parte 2:
+
+let produto = {
+nome: 'Camisa',
+cor: 'Preta',
+preco: 79.90,
+estoque: 50
+};
+
+A)
+produto.nome
+
+B)
+produto['preco']
+
+C)
+produto.estoque = 80;
+
+D)
+for (let propriedade in produto) {
+console.log(propriedade + ': ' + produto[propriedade]);
+}
